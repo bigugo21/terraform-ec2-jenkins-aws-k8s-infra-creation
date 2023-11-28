3bbcfd95a1d109a8bf0cf87c8b755ff145d8b7ea
bigugo21 <101026126+bigugo21@users.noreply.github.com>
2023-11-28 09:44:53 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -93,7 +93,7 @@ resource "aws_iam_role" "test_role" {
 EOF
 }
 
-resource "aws_iam_instance_profile" "testing_profile" {
+resource "aws_iam_instance_profile" "test_profile" {
   name = "testing_profile"
   role = "${aws_iam_role.test_role.name}"
 }
